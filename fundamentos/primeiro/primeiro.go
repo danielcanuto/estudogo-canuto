941330ac07bdd12aae6975f5b0c0a78fb6c9e664
Daniel Canuto <danielpscanuto83@gmail.com>
2021-03-08 09:02:08 -0300

    primeiro push

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,15 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+Os codigos em go são organizados em paoctes
+e para usa-los é necessario declarar um ou varios umporte
+*/
+import "fmt"
+
+// a porta de entrada  de um programa Gp é a função main
+func main() {
+	fmt.Print("Primeiro 	")
+	fmt.Print("Programa!")
+
+}
